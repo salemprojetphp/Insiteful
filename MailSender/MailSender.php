89bab67ddf584968262dad6c7d86f63f90ff17b2
salmasgh <salmasghaier74@gmail.com>
2024-03-23 17:19:35 +0100

    MailSending

---
+++ MailSender/MailSender.php
@@ -0,0 +1,63 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+class MailSender{
+    public static function sendMail($emails, $subject, $body, $attachments = []) {
+        // Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+        
+        try {
+            // Server settings
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            $mail->Host       = 'smtp.gmail.com';                        // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                    // Enable SMTP authentication
+            $mail->Username   = 'insitefulcontact@gmail.com';            // SMTP username
+            $mail->Password   = 'lpgxakcemjcnrqjf';                      // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;             // Enable implicit TLS encryption
+            $mail->Port       = 465;                                     // TCP port to connect to
+    
+            // Set sender
+            $mail->setFrom('insitefulcontact@gmail.com', 'Mailer');
+            
+            // Add recipients
+            foreach ($emails as $email) {
+                $mail->addAddress($email);
+            }
+    
+            // Add attachments
+            foreach ($attachments as $attachment) {
+                if (is_array($attachment)) {
+                    $mail->addAttachment($attachment['path'], $attachment['name']);  
+                } else {
+                    $mail->addAttachment($attachment);
+                }
+            }
+    
+            // Content
+            $mail->isHTML(true);                                         // Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $body;
+    
+            // Send email
+            $mail->send();
+        } catch (Exception $e) {
+            // Handle exception
+            // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+    
+}
+?>
