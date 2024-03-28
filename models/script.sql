51fe4de627212087ea81697636fa5ae61c522d36
azizamari <azizamari2003@gmail.com>
2024-03-28 17:58:58 +0100

    add example visitors table

--- models/script.sql
+++ models/script.sql
@@ -15,3 +15,40 @@ CREATE TABLE post(
     date DATE,
     FOREIGN KEY (Author) REFERENCES users(id)
 );
+
+CREATE TABLE visitors(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    website VARCHAR(255) NOT NULL,
+    ip VARCHAR(255) NOT NULL,
+    date DATE,
+    referrer VARCHAR(255) DEFAULT 'Direct',
+    country VARCHAR(255),
+    device ENUM('Computer', 'Phone') DEFAULT 'Computer',
+    browswer ENUM('Chrome', 'Firefox', 'Safari', 'Opera', 'Edge', 'Other') DEFAULT 'Other',
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+-- 20 Examples 
+INSERT INTO visitors (user_id, website, ip, date, referrer, country, device, browswer)
+VALUES (1, 'insat.tn', '192.168.1.1', '2024-03-27', 'Facebook', 'Country A', 'Computer', 'Chrome'),
+       (1, 'website.tn', '192.168.1.2', '2024-03-26', 'Instagram', 'Country B', 'Phone', 'Firefox'),
+       (1, 'insat.tn', '192.168.1.3', '2024-03-25', 'Direct', 'Country A', 'Computer', 'Safari'),
+       (1, 'insat.tn', '192.168.1.4', '2024-03-24', 'Google', 'Country B', 'Phone', 'Opera'),
+       (1, 'website.tn', '192.168.1.5', '2024-03-23', 'Twitter', 'Country A', 'Computer', 'Edge'),
+       (1, 'website.tn', '192.168.1.6', '2024-03-22', 'Direct', 'Country B', 'Phone', 'Other'),
+       (1, 'insat.tn', '192.168.1.7', '2024-03-21', 'Facebook', 'Country A', 'Computer', 'Chrome'),
+       (1, 'website.tn', '192.168.1.8', '2024-03-20', 'Instagram', 'Country B', 'Phone', 'Firefox'),
+       (1, 'insat.tn', '192.168.1.9', '2024-03-19', 'Direct', 'Country A', 'Computer', 'Safari'),
+       (1, 'insat.tn', '192.168.1.10', '2024-03-18', 'Google', 'Country B', 'Phone', 'Opera');
+       (1, 'website.tn', '192.168.1.11', '2024-03-17', 'Twitter', 'Country A', 'Computer', 'Edge'),
+       (1, 'insat.tn', '192.168.1.12', '2024-03-16', 'Direct', 'Country B', 'Phone', 'Other'),
+       (1, 'insat.tn', '192.168.1.13', '2024-03-15', 'Facebook', 'Country A', 'Computer', 'Chrome'),
+       (1, 'website.tn', '192.168.1.14', '2024-03-14', 'Instagram', 'Country B', 'Phone', 'Firefox'),
+       (1, 'insat.tn', '192.168.1.15', '2024-03-13', 'Direct', 'Country A', 'Computer', 'Safari'),
+       (1, 'insat.tn', '192.168.1.16', '2024-03-12', 'Google', 'Country B', 'Phone', 'Opera'),
+       (1, 'website.tn', '192.168.1.17', '2024-03-11', 'Twitter', 'Country A', 'Computer', 'Edge'),
+       (1, 'website.tn', '192.168.1.18', '2024-03-10', 'Direct', 'Country B', 'Phone', 'Other'),
+       (1, 'insat.tn', '192.168.1.19', '2024-03-09', 'Facebook', 'Country A', 'Computer', 'Chrome'),
+       (1, 'website.tn', '192.168.1.20', '2024-03-08', 'Instagram', 'Country B', 'Phone', 'Firefox');
